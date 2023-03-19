0d8e572c6e06b75a3026d05bda7a5536867ee2d4
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-03-19 13:42:49 -0300

    feat: hold part

--- src/scripts/play_area.gd
+++ src/scripts/play_area.gd
@@ -3,10 +3,14 @@ extends Node2D
 var types = ['i', 'j', 'l', 'o', 's', 't', 'z']
 
 
-func generate_new_part():
-	var new_part = load('res://scenes/parts/' + Global.next_part + '.tscn').instance()
-	new_part.part_type = Global.next_part
+func load_part(part_type):
+	var new_part = load('res://scenes/parts/' + part_type + '.tscn').instance()
+	new_part.part_type = part_type
 	add_child(new_part)
+
+
+func generate_new_part():
+	load_part(Global.next_part)
 
 	var next_part_type = types[randi() % types.size()]
 	Global.next_part = next_part_type
@@ -46,6 +50,24 @@ func check_full_line():
 				Global.inactive_positions.erase(current_position)
 				current_block.position.y += 54
 				Global.inactive_positions[Vector2(collumn, upper_line+1)] = current_block
+
+
+func _input(event):
+	if event.is_action_pressed('hold_part'):
+		var current_part = null
+
+		for child in get_children():
+			if child.is_active:
+				current_part = child
+				break
+
+		if Global.holding_part != null:
+			load_part(Global.holding_part)
+		else:
+			generate_new_part()
+
+		Global.holding_part = current_part.part_type
+		current_part.queue_free()
 
 
 func _ready():
--- src/scripts/playing.gd
+++ src/scripts/playing.gd
@@ -5,4 +5,7 @@ func _process(_delta):
 	if Global.next_part != null:
 		$next.texture = load('res://assets/parts/' + Global.next_part + '.png')
 
+	if Global.holding_part != null:
+		$holding.texture = load('res://assets/parts/' + Global.holding_part + '.png')
+
 	$score.text = str(Global.score)
